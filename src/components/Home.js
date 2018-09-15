616cb503f320bb77081a2734ae2582cb5f60010a
Biyun Wu <beenwu2016@gmail.com>
2018-09-15 15:28:49 -0400

    Add responsive image to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
 import React, { PureComponent } from 'react'
-import TJ from './../imgs/tj.jpg'
 
 export default class Home extends PureComponent {
     render () {
         return (
-            <div style={{textAlign: 'center'}}>
-                <img src={TJ} alt='Tao Jiang' style={{width: '100%'}}/>
+            <div id='background-image'>
+                {/* <img src={TJ} alt='Tao Jiang' style={{width: '50%'}}/> */}
             </div>
         )
     }
