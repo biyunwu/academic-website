c04bf72d0cf9132cfd3f12eb00fec078702accf2
Biyun Wu <beenwu2016@gmail.com>
2018-09-22 00:15:04 -0400

    Update About page

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,31 @@ export default class About extends PureComponent {
         return (
             <main className='about'>
                 <div className='info'>
-                    {/* <h2>Tao Jiang</h2> */}
-                    <img className='info-pic' src={infoPic} alt='Tao Jiang' />
+                    <h2>About Tao Jiang</h2>
+                    <div className='pic-container'>
+                        <img className='info-pic' src={infoPic} alt='Tao Jiang' />
+                    </div>
                     <div>
                         <p className='detail-info'>
-                            Tao Jiang is associate professor in the <a target='_blank' rel="noopener noreferrer" href='https://religion.rutgers.edu/graduate/graduate-faculty/892-tao-jiang-4'>Religion Department</a> at Rutgers University, New Brunswick, New Jersey, US, where he is also serving as director of the <a target='_blank' rel="noopener noreferrer" href='https://rccs.rutgers.edu/'>Rutgers Center for Chinese Studies</a>. His primary research interest is classical Chinese philosophy, Mahāyāna Buddhist philosophy, and comparative philosophy. He is the author of Contexts and Dialogue: Yogācāra Buddhism and Modern Psychology on the Subliminal Mind and the co-editor of The Reception and Rendition of Freud in China: China’s Freudian Slip. His articles have appeared in Philosophy East & West, Journal of the American Academy of Religion, Journal of Chinese Philosophy, Journal of Indian Philosophy, Dao and several anthologies. He co-directs the Rutgers Workshop on Chinese Philosophy and co-chairs the Neo-Confucian Studies Seminar at Columbia University.
+                            Tao J<span id='lastname'>IANG</span> is a scholar of classical Chinese philosophy, Mahāyāna Buddhist philosophy, and comparative philosophy. He is associate professor in the <a target='_blank' rel="noopener noreferrer" href='https://religion.rutgers.edu/graduate/graduate-faculty/892-tao-jiang-4'>Department of Religion</a> and an associate member of the graduate faculty in the Department of Philosophy at Rutgers, the State University of New Jersey in New Brunswick, US. He is director of the multidisciplinary <a target='_blank' rel="noopener noreferrer" href='https://rccs.rutgers.edu/'>Rutgers Center for Chinese Studies</a>.
+                        </p>
+                        <p className='detail-info'>
+                            Jiang is the author of <span className='italic'>Contexts and Dialogue: Yogācāra Buddhism and Modern Psychology on the Subliminal Mind</span> and the co-editor of <span className='italic'>The Reception and Rendition of Freud in China: China’s Freudian Slip</span>. His articles have appeared in <span className='italic'>Philosophy East & West</span>, <span className='italic'>Journal of the American Academy of Religion</span>, <span className='italic'>Journal of Chinese Philosophy</span>, <span className='italic'>Journal of Indian Philosophy</span>, <span className='italic'>Dao</span> and several anthologies. Currently he is finishing a monograph on classical Chinese philosophy.
+                        </p>
+                        <p className='detail-info'>
+                            Jiang co-directs the Rutgers Workshop on Chinese Philosophy and co-chairs the Neo-Confucian Studies Seminar at Columbia University. He is currently serving on the program committee of the American Academy of Religion.
                         </p>
                     </div>
                     <div className='clear'></div>
+                    {/* <div className='clear'></div> */}
+                </div>
+                <div className='contact'>
+                    <h2>Contact Info</h2>
+                    <address>
+                        <p>Rutgers University</p>
+                        <p>70 Lipman Drive<br/>Loree Hall<br/>Room 140<br/>New Brunswick<br/>NJ 08901-8525</p>
+                        <p><a href='mailto:tjiang@rutgers.edu'>tjiang@rutgers.edu</a></p>
+                    </address>
                 </div>
             </main>
         )
--- src/components/Papers.js
+++ src/components/Papers.js
@@ -3,8 +3,8 @@ import React, { PureComponent } from 'react';
 export default class Papers extends PureComponent {
     render () {
         return (
-            <div>
-            </div>
+            <main>
+            </main>
         )
     }
 }
