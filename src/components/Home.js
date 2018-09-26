189373133e9867fb08971c29e58723f50d847197
Biyun Wu <beenwu2016@gmail.com>
2018-09-25 22:21:55 -0400

    Modify images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React, { PureComponent } from 'react'
+// import BackgroundImage from './../imgs/tj.jpg'
 
 export default class Home extends PureComponent {
     render () {
-        // const {backgroundImage} = this.props
+        const {backgroundImage} = this.props
 
         return (
             <main
                 id='background-image'
-                // style={{backgroundImage: `url(${backgroundImage})`}}
+                style={{backgroundImage: `url("${backgroundImage}")`}}
+                // style={{backgroundImage: `url(${BackgroundImage})`}}
+                // style={{backgroundImage: `url('https://raw.githubusercontent.com/biyunwu/academic-website/master/src/imgs/tj.jpg')`}}
             >
+                <p style={{visibility: 'hidden'}}>Background image</p>
             </main>
         )
     }
