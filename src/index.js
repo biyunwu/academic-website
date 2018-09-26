bcd67e62e1a29e36f50d82af0bf67467aee94fad
Biyun Wu <beenwu2016@gmail.com>
2018-09-25 20:32:43 -0400

    Use react-snap for prerendering

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { hydrate, render } from "react-dom"; // For prerendering
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>,
-    document.getElementById('root')
-)
+// ReactDOM.render(
+//     <BrowserRouter><App /></BrowserRouter>,
+//     document.getElementById('root')
+// )
+
+// registerServiceWorker();
+
+
+// For prerendering
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+    hydrate(<BrowserRouter><App /></BrowserRouter>, rootElement);
+} else {
+    render(<BrowserRouter><App /></BrowserRouter>, rootElement);
+}
 
 registerServiceWorker();
