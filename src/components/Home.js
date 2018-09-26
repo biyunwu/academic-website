8d63042df0273027fab3dc142e907dafb7676ee7
Biyun Wu <beenwu2016@gmail.com>
2018-09-25 21:01:46 -0400

    Specify home background image in SASS

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,12 @@ import React, { PureComponent } from 'react'
 
 export default class Home extends PureComponent {
     render () {
-        const {backgroundImage} = this.props
+        // const {backgroundImage} = this.props
 
         return (
             <main
                 id='background-image'
-                style={{backgroundImage: `url(${backgroundImage})`}}
+                // style={{backgroundImage: `url(${backgroundImage})`}}
             >
             </main>
         )
