25d9d0b95e742c4b3f9dcc443283deae96dbfc2b
Biyun Wu <beenwu2016@gmail.com>
2018-09-27 14:17:50 -0400

    Use 'spa-github-pages' to deal with refresh request in React SPA

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react'
-// import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { hydrate, render } from "react-dom"; // For prerendering
+// import { hydrate, render } from "react-dom"; // For prerendering
 import registerServiceWorker from './registerServiceWorker'
 
-// ReactDOM.render(
-//     <BrowserRouter><App /></BrowserRouter>,
-//     document.getElementById('root')
-// )
-
-// registerServiceWorker();
+ReactDOM.render(
+    <BrowserRouter><App /></BrowserRouter>,
+    document.getElementById('root')
+)
 
 
 // For prerendering
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-    hydrate(<BrowserRouter><App /></BrowserRouter>, rootElement);
-} else {
-    render(<BrowserRouter><App /></BrowserRouter>, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//     hydrate(<BrowserRouter><App /></BrowserRouter>, rootElement);
+// } else {
+//     render(<BrowserRouter><App /></BrowserRouter>, rootElement);
+// }
 
 registerServiceWorker();
