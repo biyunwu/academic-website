74260840c383e465a597698bab3d09d8b4beaea8
Biyun Wu <beenwu2016@gmail.com>
2018-10-07 03:48:39 -0400

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
+
+import ReactGA from 'react-ga'
 import 'react-app-polyfill/ie9'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App'
 // import { hydrate, render } from "react-dom"; // For prerendering
 import registerServiceWorker from './registerServiceWorker'
 
+
+// Google Analytics reference: https://github.com/react-ga/react-ga/issues/122#issuecomment-353101102
+ReactGA.initialize('UA-114112626-2')
+class Analytics extends React.Component {
+    componentDidMount() {
+        this.sendPageChange(this.props.location.pathname, this.props.location.search)
+    }
+
+    componentDidUpdate (prevProps) {
+        if (this.props.location.pathname !== prevProps.location.pathname
+            || this.props.location.search !== prevProps.location.search) {
+            this.sendPageChange(this.props.location.pathname, this.props.location.search)
+        }
+    }
+
+    sendPageChange (pathname, search) {
+        const page = pathname + search
+        ReactGA.set({page});
+        ReactGA.pageview(page);
+    }
+
+    render() {
+        return null
+    }
+}
+
+const AnalyticsTracker = () => <Route component={Analytics} />
+
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>,
+    <BrowserRouter>
+        <React.Fragment>
+            <AnalyticsTracker />
+            <App />
+        </React.Fragment>
+    </BrowserRouter>,
     document.getElementById('root')
 )
-
 
 // For prerendering
 // const rootElement = document.getElementById("root");
